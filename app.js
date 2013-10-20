3e6a63cc993f33df4f218586cf121e63d45d94a1
joster422 <joster422@gmail.com>
2013-10-19 22:27:16 -0400

    Style Me!

--- app.js
+++ app.js
@@ -4,67 +4,74 @@
 	var memoryGameApp = angular.module('memoryGameApp', []);
 
 	memoryGameApp.controller('gameCtrl', function($scope, $timeout) {
+		var highlightTile;	//Stupid "global" variable.
+
+		var tile = function(selected, color) {
+			this.selected = selected;
+			this.color = color;
+		};
+
+		var randomColor = function() {
+			return Math.floor(Math.random()*16777215).toString(16);
+		};
+
+		var createGrid = function() {
+			var grid = [];
+			for(var i = 0; i < $scope.game.size; i++) {
+				grid[i] = [];
+				for(var j = 0; j < $scope.game.size; j++) {
+					grid[i][j] = new tile(false, randomColor());
+				}
+			}
+			return grid;
+		};
+
 		var gameConstuctor = function(){
 			return {
-				showMenu : true,
-				showBoard : false,
-				showRetry : false,
-				grid : [
-					[false, false],
-					[false, false],
-				],
-				playerGuess : [],
-				correct : [],
-				clicks : 6,
+				showMenu      : true,
+				showBoard     : false,
+				showRetry     : false,
+				playerGuess   : [],
+				correct       : [],
+				clicks        : 5,
 				clicksAllowed : false,
 				size : 2,
-				message : ""
+				//grid : [
+				//	[false, false],
+				//	[false, false],
+				//]
+				gird : []
 			};
 		};
 		$scope.game = gameConstuctor();
 
-		$scope.option1Construct = [1, 2, 3];
-		$scope.option1 = function(id) {
-			switch(id) {
-				case 0:
-					$scope.game.clicks = 6;
-					return;
-				case 1:
-					$scope.game.clicks = 12;
-					return;
-				case 2:
-					$scope.game.clicks = 24;
-					return;
+		$scope.message = "";
+
+		$scope.option1 = [
+			{text : "Easy", tileClicks : 5, selected : true},
+			{text : "Medium", tileClicks : 10, selected : false},
+			{text : "Difficult", tileClicks : 15, selected : false}
+		];
+		$scope.option2 = [
+			{text : "Small", gridLength : 2, selected : true},
+			{text : "Normal", gridLength : 3, selected : false},
+			{text : "Large", gridLength : 4, selected : false}
+		];
+
+		$scope.option1Choice = function(id) {
+			$scope.game.clicks = $scope.option1[id].tileClicks;
+
+			for(var a = 0; a < $scope.option1.length; a++) {
+				a === id ? $scope.option1[a].selected = true : $scope.option1[a].selected = false;
 			}
-		}
-		$scope.option2 = function(id) {
-			switch(id) {
-				case 0:
-					$scope.game.size = 2;
-					$scope.game.grid = [
-						[false, false],
-						[false, false]
-					];
-					return;
-				case 1:
-					$scope.game.size = 3;
-					$scope.game.grid = [
-						[false, false, false],
-						[false, false, false],
-						[false, false, false]
-					];
-					return;
-				case 2:
-					$scope.game.size = 4;
-					$scope.game.grid = [
-						[false, false, false, false],
-						[false, false, false, false],
-						[false, false, false, false],
-						[false, false, false, false]
-					];
-					return;
+		};
+		$scope.option2Choice = function(id) {
+			$scope.game.size = $scope.option2[id].gridLength;
+
+			for(var a = 0; a < $scope.option2.length; a++) {
+				a === id ? $scope.option2[a].selected = true : $scope.option2[a].selected = false;
 			}
-		}
+		};
 
 		var showPattern = function(){
 			var counter = 0,
@@ -76,7 +83,9 @@
 					var show = $scope.game.correct[iter];
 					$scope.resetGrid();
 					console.log(Math.floor(show / $scope.game.size) + " and " + show % $scope.game.size);
-					$scope.game.grid[Math.floor(show / $scope.game.size)][show % $scope.game.size] = true;
+					$timeout(function() {
+						$scope.game.grid[Math.floor(show / $scope.game.size)][show % $scope.game.size].selected = true;
+					}, 50);
 					iter++;
 				}, counter);
 			}
@@ -94,31 +103,39 @@
 				$scope.game.showRetry = false;
 			}
 			else {
-				$scope.game.message = "Get Ready";
+				$scope.game.showRetry = false;
 				$scope.game.showMenu = false;
+				$scope.game.grid = createGrid();
+				$scope.message = "Get Ready";
+				$scope.game.playerGuess = [];
+				$scope.game.correct = [];
 				$timeout(function(){
 					$scope.game.showBoard = true;
 					$scope.game.showRetry = false;
 					showPattern();
-					$scope.game.message = "";
+					$scope.message = "";
 				}, 1000);
 			}	
 		};
 
 		$scope.resetGrid = function() {
 			for(var i = 0; i < $scope.game.size; i++) {
-				for(var j = 0; j < $scope.game.size; j++) 
-					$scope.game.grid[i][j] = false;
+				for(var j = 0; j < $scope.game.size; j++) {
+					$scope.game.grid[i][j].selected = false;
+				}
 			}		
 		};
 
 		var didWin = function(){
 			for(var i = 0; i < $scope.game.playerGuess.length; i++) {
 				if($scope.game.playerGuess[i] !== $scope.game.correct[i]) {
+					$timeout.cancel(highlightTile);
+					$scope.resetGrid();
 					$scope.game.clicksAllowed = false;
-					$scope.game.message = "Try Again";
+					$scope.game.showBoard = false;
+					$scope.message = "Try Again";
 					$timeout(function(){
-						$scope.game.message = "";
+						$scope.message = "";
 						$scope.game.showRetry = true;
 						$scope.game.showBoard = false;
 					}, 1000);
@@ -126,18 +143,31 @@
 				}
 				if(i === $scope.game.clicks - 1) {
 					$scope.game.clicksAllowed = false;
-					var ranSeed = Math.floor(Math.random() * 3);
+					$scope.game.showBoard = false;
+					var ranSeed = Math.floor(Math.random() * 7);
 					if(ranSeed === 0) {
-						$scope.game.message = "Great";
+						$scope.message = "Great";
 					}
 					else if(ranSeed === 1) {
-						$scope.game.message = "Congratulations";
+						$scope.message = "Congratulations";
 					}
-					else if(ranSeed == 2) {
-						$scope.game.message = "Excellent";
+					else if(ranSeed === 2) {
+						$scope.message = "Excellent";
+					}
+					else if(ranSeed === 3) {
+						$scope.message = "Wonderful";
+					}
+					else if(ranSeed === 4) {
+						$scope.message = "Astounding";
+					}
+					else if(ranSeed === 5) {
+						$scope.message = "Spectacular";
+					}
+					else if(ranSeed === 6) {
+						$scope.message = "Genius";
 					}
 					$timeout(function(){
-						$scope.game.message = "";
+						$scope.message = "";
 						$scope.changeViews(true);
 					}, 1500);
 					return;
@@ -145,16 +175,14 @@
 			}
 		};
 
-		$scope.randomColor = function() {
-			return {'background-color' : '#' + Math.floor(Math.random()*16777215).toString(16)};
-		};
-
 		$scope.clickTile = function(i1, i2) {
 			if($scope.game.clicksAllowed) {
 				$scope.resetGrid();
-				$scope.game.grid[i1][i2] = true;
-				$scope.playerGuess.push((i1 * $scope.game.size) + i2);	
-				didWin();
+				$scope.game.playerGuess.push((i1 * $scope.game.size) + i2);	
+				highlightTile = $timeout(function() {
+					$scope.game.grid[i1][i2].selected = true;
+					didWin();
+				}, 50);
 			}
 			for(var i = 0; i < $scope.game.playerGuess.length; i++) {
 				console.log($scope.game.playerGuess[i] + " ");
